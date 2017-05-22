713019288af26b74e1f7e6885fdfa80bba370cad
shintautamidewi <shintautamidewi20@gmail.com>
2017-05-22 22:50:29 +0700

    Add files via upload

---
+++ shinta utami 14111202 encapintegration/shinta.java
@@ -0,0 +1,17 @@
+
+package shinta;
+
+public class Shinta {
+
+    
+    public static void main(String[] args) {
+ Handphone data = new handphone ("putih","nokia","z50");
+  System.out.println("warna hp     :"+data.warna);
+  System.out.println("nama hp     :"+ data.getnama());
+  System.out.println("type hp         :"+ data.type);
+   
+    
+    }
+   
+    
+}
